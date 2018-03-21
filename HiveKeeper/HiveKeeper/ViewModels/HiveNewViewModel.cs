342228d1baadcd689870b87819c476ecd70600e4
Zohrab Broyan <31774934+zbroyan@users.noreply.github.com>
2018-03-20 22:10:37 -0700

    adding new hive

--- HiveKeeper/HiveKeeper/ViewModels/HiveNewViewModel.cs
+++ HiveKeeper/HiveKeeper/ViewModels/HiveNewViewModel.cs
@@ -1,11 +1,34 @@
-﻿using System;
+﻿using HiveKeeper.Models;
+using HiveKeeper.Views;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using Xamarin.Forms;
 
 namespace HiveKeeper.ViewModels
 {
     public class HiveNewViewModel : BaseViewModel
     {
+        public HiveNewViewModel()
+        {
+            MessagingCenter.Subscribe<HiveAddNewPage>(this, Messages.HIVE_ADDED, async (sender) =>
+            {
+                var _item = GetHiveModel();
+                MessagingCenter.Send<HiveNewViewModel, Hive>(this, Messages.HIVE_ADDED, _item);
+            });
+        }
 
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        private Hive GetHiveModel()
+        {
+            return new Hive
+            {
+                Id = Id,
+                Name = Name,
+            };
+        }
     }
 }
